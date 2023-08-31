9a0d25e7938efc47f82beeede922815506a583cc
Dzmitry Ramaniuk <dzmitry.ramaniuk@godel.shellenergy.co.uk>
2023-09-01 00:05:30 +0200

    Components

--- src/index.js
+++ src/index.js
@@ -3,22 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-// const elem = <h2>Hello World!</h2>;
-
-const text = 'Hello World';
-
-const elem = (
-  <div>
-    <h2>{text}</h2>
-    <input type="text" />
-    <button>test</button>
-  </div>
-)
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    {elem}
-  </React.StrictMode>
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
