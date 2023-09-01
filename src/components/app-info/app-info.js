0bff738642b30e604e02abdeb03eaba4499ecea2
Dzmitry Ramaniuk <dzmitry.ramaniuk@godel.shellenergy.co.uk>
2023-09-01 17:50:16 +0200

    Add components templates

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Employers check in company</h1>
+            <h2>Number of employees: </h2>
+            <h2>Number of increased salaries: </h2>
+        </div>
+    );
+};
+
+export default AppInfo;
