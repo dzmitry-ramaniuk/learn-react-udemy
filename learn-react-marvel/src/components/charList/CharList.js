bcdcf0f0035f83807cc3a273187260cd7ed2fab0
Dzmitry Ramaniuk <dzmitry.ramaniuk@gmail.com>
2023-09-13 16:33:24 +0200

    Add focus in char list

--- learn-react-marvel/src/components/charList/CharList.js
+++ learn-react-marvel/src/components/charList/CharList.js
@@ -63,15 +63,42 @@ class CharList extends Component {
         });
     };
 
+    itemRefs = [];
+
+    setRef = (ref) => {
+        this.itemRefs.push(ref);
+    };
+
+    focusOnItem = (id) => {
+        this.itemRefs.forEach((item) => item.classList.remove("char__item_selected"));
+        this.itemRefs[id].classList.add("char__item_selected");
+        this.itemRefs[id].focus();
+    };
+
     renderItems = (arr) => {
-        const items = arr.map((item) => {
+        const items = arr.map((item, i) => {
             let imgStyle = { objectFit: "cover" };
             if (item.thumbnail === "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg") {
                 imgStyle = { objectFit: "unset" };
             }
 
             return (
-                <li className="char__item" key={item.id} onClick={() => this.props.onCharSelected(item.id)}>
+                <li
+                    className="char__item"
+                    key={item.id}
+                    tabIndex={i}
+                    onClick={() => {
+                        this.props.onCharSelected(item.id);
+                        this.focusOnItem(i);
+                    }}
+                    onKeyUp={(e) => {
+                        if (e.key === " " || e.key === "Enter") {
+                            this.props.onCharSelected(item.id);
+                            this.focusOnItem(i);
+                        }
+                    }}
+                    ref={this.setRef}
+                >
                     <img src={item.thumbnail} alt={item.name} style={imgStyle} />
                     <div className="char__name">{item.name}</div>
                 </li>
