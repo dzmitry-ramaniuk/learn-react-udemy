c8edda56661a7f04c9b65bbcaba8dd2dbe4d0b76
Dzmitry Ramaniuk <dzmitry.ramaniuk@gmail.com>
2023-09-14 17:28:40 +0200

    Add hook for http request

--- learn-react-marvel/src/components/charInfo/CharInfo.js
+++ learn-react-marvel/src/components/charInfo/CharInfo.js
@@ -2,43 +2,30 @@ import { useState, useEffect } from "react";
 import Spinner from "../spinner/Spinner";
 import ErrorMessage from "../errorMessage/ErrorMessage";
 import Skeleton from "../skeleton/Skeleton";
-import MarvelService from "../../services/MarvelService";
+import useMarvelService from "../../services/MarvelService";
 import "./charInfo.scss";
 
 const CharInfo = (props) => {
     const [char, setChar] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
 
-    const marvelService = new MarvelService();
+    const {loading, error, getCharacter, clearError} = useMarvelService();
 
     useEffect(() => {
         updateChar();
     }, [props.charId]);
 
     const updateChar = () => {
+        clearError();
         const { charId } = props;
         if (!charId) {
             return;
         }
 
-        onCharLoading();
-        marvelService.getCharacter(charId).then(onCharLoaded).catch(onError);
+        getCharacter(charId).then(onCharLoaded);
     };
 
     const onCharLoaded = (char) => {
         setChar(char);
-        setLoading(false);
-        setError(false);
-    };
-
-    const onCharLoading = () => {
-        setLoading(true);
-    };
-
-    const onError = () => {
-        setLoading(false);
-        setError(true);
     };
 
     const skeleton = char || loading || error ? null : <Skeleton />;
--- learn-react-marvel/src/components/charList/CharList.js
+++ learn-react-marvel/src/components/charList/CharList.js
@@ -1,31 +1,25 @@
 import { useState, useEffect, useRef } from "react";
 import Spinner from "../spinner/Spinner";
 import ErrorMessage from "../errorMessage/ErrorMessage";
-import MarvelService from "../../services/MarvelService";
+import useMarvelService from "../../services/MarvelService";
 
 import "./charList.scss";
 
 const CharList = (props) => {
     const [charList, setCharList] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(false);
     const [newItemLoading, setNewItemLoading] = useState(false);
     const [offset, setOffset] = useState(210);
     const [charEnded, setCharEnded] = useState(false);
 
-    const marvelService = new MarvelService();
+    const { loading, error, getAllCharacters } = useMarvelService();
 
     useEffect(() => {
-        onRequest();
+        onRequest(offset, true);
     }, []);
 
-    const onRequest = (offset) => {
-        onCharListLoading();
-        marvelService.getAllCharacters(offset).then(onCharListLoaded).catch(onError);
-    };
-
-    const onCharListLoading = () => {
-        setNewItemLoading(true);
+    const onRequest = (offset, initial) => {
+        setNewItemLoading(!initial);
+        getAllCharacters(offset).then(onCharListLoaded);
     };
 
     const onCharListLoaded = (newCharList) => {
@@ -42,16 +36,9 @@ const CharList = (props) => {
         let filteredCharList = [...new Map([...mergedCharListMap]).values()];
 
         setCharList(filteredCharList);
-        setLoading(false);
-        setError(false);
         setNewItemLoading(false);
         setOffset((offset) => offset + 9);
         setCharEnded((charEnded) => ended);
-    };
-
-    const onError = () => {
-        setError(true);
-        setLoading(false);
     };
 
     const itemRefs = useRef([]);
@@ -98,14 +85,13 @@ const CharList = (props) => {
     const items = renderItems(charList);
 
     const errorMessage = error ? <ErrorMessage /> : null;
-    const spinner = loading ? <Spinner /> : null;
-    const content = !(loading || error) ? items : null;
+    const spinner = loading && !newItemLoading ? <Spinner /> : null;
 
     return (
         <div className="char__list">
             {errorMessage}
             {spinner}
-            {content}
+            {items}
 
             <button
                 className="button button__main button__long"
