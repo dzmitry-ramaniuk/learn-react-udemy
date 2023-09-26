b3eb0ff1a0007e15283b6376d58fa6b7db617748
Dzmitry Ramaniuk <dzmitry.ramaniuk@gmail.com>
2023-09-26 18:16:32 +0200

    add useCallback for onDelete

--- hero_admin_panel_template/src/components/heroesList/HeroesList.js
+++ hero_admin_panel_template/src/components/heroesList/HeroesList.js
@@ -1,11 +1,15 @@
-import {useHttp} from '../../hooks/http.hook';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useHttp } from "../../hooks/http.hook";
+import { useEffect, useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-import { heroesFetching, heroesFetched, heroesFetchingError } from '../../actions';
+import {
+    heroesFetching,
+    heroesFetched,
+    heroesFetchingError,
+} from "../../actions";
 import HeroesListItem from "../heroesListItem/HeroesListItem";
-import Spinner from '../spinner/Spinner';
-import { heroesDelete } from '../../actions';
+import Spinner from "../spinner/Spinner";
+import { heroesDelete } from "../../actions";
 
 // Задача для этого компонента:
 // При клике на "крестик" идет удаление персонажа из общего состояния
@@ -13,46 +17,54 @@ import { heroesDelete } from '../../actions';
 // Удаление идет и с json файла при помощи метода DELETE
 
 const HeroesList = () => {
-    const {filteredHeroes, heroesLoadingStatus} = useSelector(state => state);
+    const { filteredHeroes, heroesLoadingStatus } = useSelector(
+        (state) => state,
+    );
     const dispatch = useDispatch();
-    const {request} = useHttp();
+    const { request } = useHttp();
 
     useEffect(() => {
         dispatch(heroesFetching());
         request("http://localhost:3001/heroes")
-            .then(data => dispatch(heroesFetched(data)))
-            .catch(() => dispatch(heroesFetchingError()))
+            .then((data) => dispatch(heroesFetched(data)))
+            .catch(() => dispatch(heroesFetchingError()));
 
         // eslint-disable-next-line
     }, []);
 
-    const onDelete = (id) => {
-        request(`http://localhost:3001/heroes/${id}`, 'DELETE')
-         .then(() => dispatch(heroesDelete(id)))
-    }
+    const onDelete = useCallback(
+        (id) => {
+            request(`http://localhost:3001/heroes/${id}`, "DELETE").then(() =>
+                dispatch(heroesDelete(id)),
+            );
+        },
+        [request],
+    );
 
     if (heroesLoadingStatus === "loading") {
-        return <Spinner/>;
+        return <Spinner />;
     } else if (heroesLoadingStatus === "error") {
-        return <h5 className="text-center mt-5">Ошибка загрузки</h5>
+        return <h5 className="text-center mt-5">Ошибка загрузки</h5>;
     }
 
     const renderHeroesList = (arr) => {
         if (arr.length === 0) {
-            return <h5 className="text-center mt-5">Героев пока нет</h5>
+            return <h5 className="text-center mt-5">Героев пока нет</h5>;
         }
 
-        return arr.map(({id, ...props}) => {
-            return <HeroesListItem key={id} {...props} onDelete={() => onDelete(id)}/>
-        })
-    }
+        return arr.map(({ id, ...props }) => {
+            return (
+                <HeroesListItem
+                    key={id}
+                    {...props}
+                    onDelete={() => onDelete(id)}
+                />
+            );
+        });
+    };
 
     const elements = renderHeroesList(filteredHeroes);
-    return (
-        <ul>
-            {elements}
-        </ul>
-    )
-}
+    return <ul>{elements}</ul>;
+};
 
 export default HeroesList;
